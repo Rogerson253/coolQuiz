30e924673b75b76ced8f99ad626a739500df24e9
Rogerson Jean-Charles <Rogerson253@Gmail.com>
2020-02-29 20:47:41 -0600

    created functions that start the game and change questions

--- scripts.js
+++ scripts.js
@@ -1,5 +1,10 @@
 var countD = document.querySelector(".time");
+var clearDiv = document.querySelector(".jumbotron");
 var btn = document.querySelector("#btn");
+var cardOne = document.querySelector(".cardOne");
+var cardTwo = document.querySelector(".cardTwo");
+var cardThree = document.querySelector(".cardThree");
+
 
 var remaining = 10; 
     
@@ -10,12 +15,51 @@ function setTime() {
 
         if(remaining === 0) {
             clearInterval(interval);
-            alert("Done");
         }
     }, 1000);
-}
+};
 
-btn.addEventListener("click", setTime);
+function demoDisplay() {
+    cardOne.style.display = "none";
+    cardTwo.style.display = "none";
+    cardThree.style.display = "none";
+  }
+demoDisplay();
+
+function clear() {
+    clearDiv;
+    cardOne;
+    clearDiv.parentNode.replaceChild(cardOne, clearDiv);
+    cardOne.style.display = "block";
+};
+
+function start() {
+    var startGame = btn;
+    startGame.addEventListener("click", clear);
+    startGame.addEventListener("click", setTime);
+}
+start();
+
+function clear2() {
+    cardOne;
+    cardTwo;
+    cardOne.parentNode.replaceChild(cardTwo, cardOne);
+    cardTwo.style.display = "block";
+};
+
+function clear3() {
+    cardTwo;
+    cardThree;
+    cardTwo.parentNode.replaceChild(cardThree, cardTwo);
+    cardThree.style.display = "block";
+};
+
+
+
+
+
+
+
 
 
 
